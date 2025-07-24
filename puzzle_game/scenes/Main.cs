b42b4c37f102bcfaa3dabe336462f56a5cf41b3a
Purity Ochang <axb736@usask.ca>
2025-07-24 08:08:58 -0600

    Second video complete, third video begins

--- puzzle_game/scenes/Main.cs
+++ puzzle_game/scenes/Main.cs
@@ -6,12 +6,20 @@ public partial class Main : Node2D
     //Called wehn the node enters the scene tree for the first time./
     public override void _Ready()
     {
+        var testNumber = 10;
+        var testArray = new string[] { "hi", "how", "are", "you" };
+        TestMethod();
         GD.Print("Hello world!!!");
     }
 
     // Called every fram. 'delta' is the elapsed tiime since the previous frame.
     public override void _Process(double delta)
     {
-        
+
+    }
+
+    private void TestMethod()
+    {
+        GD.Print("entered test method!");
     }
 }
